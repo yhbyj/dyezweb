2a7fb94fe185eb841ab014dfdf6a827616437cb9
yhb <6887993@qq.com>
2020-05-22 11:25:02 +0800

    ##第十一天 ### 自动化部署（legacy online） ### docker 方式部署（local online） change _deployment/docker/local/django/start.sh

--- _deployment/docker/local/django/start.sh
+++ _deployment/docker/local/django/start.sh
@@ -3,4 +3,4 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
-gunicorn dyezweb.wsgi:application -w 4 -k gthread -b 0.0.0.0:8000 --chdir=/app
+gunicorn dyezweb.wsgi:application -w 4 -k gthread -b 0.0.0.0:8000 --chdir=/projects/dyezweb
